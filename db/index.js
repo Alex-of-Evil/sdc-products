d0ef2f6811b3c4314038bfddac3e6a904b095ca4
Alex Tarnawski <amtarnawski@gmail.com>
2021-06-24 13:50:29 -0700

    fixed permission problem

---
+++ db/index.js
@@ -0,0 +1,18 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'sdc_db_1',
+  database: 'products_api',
+  password: 'postgres',
+  port: '5432',
+});
+
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
+
+module.exports = {
+  pool,
+};
