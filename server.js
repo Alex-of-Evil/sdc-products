c9c90b2e907f428b7115ff5cca8b4dbf64f4a305
Alex Tarnawski <amtarnawski@gmail.com>
2021-06-24 13:29:58 -0700

    permissions

--- server.js
+++ server.js
@@ -10,3 +10,22 @@ app.use(express.static(path.resolve(__dirname, '/public')));
 app.listen(80);
 
 console.log('Listening on port 80...');
+
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'sdc_db_1',
+//   database: 'products_api',
+//   password: 'postgres',
+//   port: '5432',
+// });
+
+// pool.on('error', (err) => {
+//   console.error('Unexpected error on idle client', err);
+//   process.exit(-1);
+// });
+
+// module.exports = {
+//   pool,
+// };
