4a02cd3642e449da75371b0cc711d2f90240ce50
Alex Tarnawski <amtarnawski@gmail.com>
2021-06-26 12:34:18 -0700

    implemented skus query

--- models/skus.js
+++ models/skus.js
@@ -1,10 +1,41 @@
 async function querySkus(client, styleId) {
+  const text = `
+    SELECT json_agg(skus_array)
+    AS skus
+    FROM (
+      SELECT json_build_object(
+        (SELECT sku),
+        (SELECT json_build_object)
+      )
+      AS skus_array
+      FROM
+      (
+        SELECT
+        id AS sku, json_build_object(
+          'size', (
+            SELECT size
+          ),
+          'quantity', (
+            SELECT quantity
+          )
+        )
+        FROM
+        (
+        SELECT id, size, quantity
+        FROM skus
+        WHERE style_id=1
+        ) AS m
+      ) AS q
+    ) as z
+  `
+
   const query = {
-    text: `SELECT id AS sku, size, quantity FROM skus WHERE style_id=$1`,
+    text,
     values: [styleId],
   };
   try {
     const response = await client.query(query);
+    console.log(response);
     return response.rows;
   } catch (err) {
     console.error(err.stack);
@@ -15,4 +46,3 @@ async function querySkus(client, styleId) {
 module.exports = {
   querySkus,
 };
-
