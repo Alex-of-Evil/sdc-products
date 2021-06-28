2bf128b9ec12625eddc32c7c1894a2c66c05c4c5
Alex Tarnawski <amtarnawski@gmail.com>
2021-06-28 12:20:33 -0700

    added DISTINCT to related query

--- models/related.js
+++ models/related.js
@@ -1,6 +1,6 @@
 async function queryRelated(client, productId) {
   const query = {
-    text: 'SELECT array_agg(related_product_id) FROM (SELECT related_product_id FROM related WHERE current_product_id=$1) AS b',
+    text: 'SELECT array_agg(related_product_id) FROM (SELECT DISTINCT related_product_id FROM related WHERE current_product_id=$1) AS b',
     values: [productId],
   };
   try {
